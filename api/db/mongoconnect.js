c7c09578c4ca79cd166633992b42d00b1df3e28b
Ayala-k <kluftayala@gmail.com>
2024-01-04 10:26:57 +0200

    h

--- api/db/mongoconnect.js
+++ api/db/mongoconnect.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
-const {config} = require("../config/secret")
+const { config } = require("../config/secret")
 
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.7nbv7ig.mongodb.net/FINAL_PROJECT`);
+    //await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.7nbv7ig.mongodb.net/FINAL_PROJECT`);
+    await mongoose.connect(
+        `mongodb+srv://${config.userDb}:${config.passDb}@cluster0.7nbv7ig.mongodb.net/FINAL_PROJECT`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    );
+
     console.log("mongo connection")
 }
